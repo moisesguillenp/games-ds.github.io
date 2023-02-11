d963acee2215dff174e358271856aa818112d7b6
games-ds <118151432+games-ds@users.noreply.github.com>
2023-02-11 15:28:24 -0300

    Add files via upload

--- meta.js
+++ meta.js
@@ -1,4 +1,23 @@
 function cambioColor(){
-    var temas = document.getElementById("body");
+    let temas = document.getElementById("body");
     temas.classList.toggle("bodyoscuro")
 }
+
+let dropdownBtn = document.querySelector(".dropdown-btn");
+let dropdownContent = document.querySelector(".dropdown-content");
+
+dropdownBtn.addEventListener("click", function() {
+  dropdownContent.classList.toggle("show");
+});
+
+window.addEventListener("click", function(event) {
+  if (!event.target.matches(".dropdown-btn")) {
+    let dropdowns = document.querySelectorAll(".dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+});
