cc9049b4d9fcab4a0a77e5416dfc9b2f4a406f98
games-ds <118151432+games-ds@users.noreply.github.com>
2023-02-27 23:11:14 -0300

    Add files via upload

---
+++ admin/meta.js
@@ -0,0 +1,24 @@
+function modificarParte() {
+    let nuevoTexto = document.getElementById("nuevo-texto").value;
+  
+    let parteModificar = document.getElementById("parte-modificar");
+  
+    parteModificar.innerHTML = nuevoTexto;
+  }
+  
+  function modificarParte1() {
+    let nuevoTexto = document.getElementById("nuevo-texto").value;
+  
+    let parteModificar = document.getElementById("parte-modificar1");
+  
+    parteModificar.innerHTML = nuevoTexto;
+  }
+  
+  function modificarParte2() {
+    let nuevoTexto = document.getElementById("nuevo-texto").value;
+  
+    let parteModificar = document.getElementById("parte-modificar2");
+  
+    parteModificar.innerHTML = nuevoTexto;
+  }
+  
