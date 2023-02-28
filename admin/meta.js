ce2107020d978a6ab07b5720b5d16b487dd25b46
games-ds <118151432+games-ds@users.noreply.github.com>
2023-02-27 23:24:07 -0300

    Update meta.js

--- admin/meta.js
+++ admin/meta.js
@@ -1,24 +1,24 @@
 function modificarParte() {
-    let nuevoTexto1 = document.getElementById("nuevo-texto1").value;
+    let nuevoTextoc = document.getElementById("nuevo-texto1").value;
   
     let parteModificar = document.getElementById("parte-modificar");
   
-    parteModificar.innerHTML = nuevoTexto;
+    parteModificar.innerHTML = nuevoTextoc;
   }
   
   function modificarParte1() {
-    let nuevoTexto2 = document.getElementById("nuevo-texto2").value;
+    let nuevoTextob = document.getElementById("nuevo-texto2").value;
   
     let parteModificarb = document.getElementById("parte-modificar1");
   
-    parteModificarb.innerHTML = nuevoTexto;
+    parteModificarb.innerHTML = nuevoTextob;
   }
   
   function modificarParte2() {
-    let nuevoTexto3 = document.getElementById("nuevo-texto3").value;
+    let nuevoTextoa = document.getElementById("nuevo-texto3").value;
   
     let parteModificara = document.getElementById("parte-modificar2");
   
-    parteModificara.innerHTML = nuevoTexto;
+    parteModificara.innerHTML = nuevoTextoa;
   }
   
